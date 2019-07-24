6ff7f6e0759becc0ef144735c4da7ea52cad4abe
Maggie Cai <garyguo110@gmail.com>
2019-07-24 14:52:18 -0700

    chromebrowser working

--- hello/views.py
+++ hello/views.py
@@ -20,43 +20,46 @@ def index(request):
     print(os.path.abspath("chromedriver"))
     driver = webdriver.Chrome(executable_path="chromedriver", chrome_options=chrome_options)
     print("starting web driver")
-    driver.implicitly_wait(30)
-    driver.maximize_window()
+    driver.implicitly_wait(3)
+    driver.get("http://rmaggiecai.com/")
+    return HttpResponse(driver.page_source)
 
-    # Navigate to the application home page
-    driver.get("http://www.google.com")
-
-    # get the search textbox
-    search_field = driver.find_element_by_id("lst-ib")
-    search_field.clear()
-
-    # enter search keyword and submit
-    search_field.send_keys("Selenium WebDriver Interview questions")
-    search_field.submit()
-
-    # get the list of elements which are displayed after the search
-    # currently on result page using find_elements_by_class_name method
-    lists= driver.find_elements_by_class_name("_Rm")
-
-    # get the number of elements found
-    print ("Found " + str(len(lists)) + " searches:")
-
-    # iterate through each element and print the text that is
-    # name of the search
-
-    i=0
-    for listitem in lists:
-       print (listitem.get_attribute("innerHTML"))
-       i=i+1
-       if(i>10):
-          break
-
-    # close the browser window
-    driver.quit()
-
-    r = requests.get('http://httpbin.org/status/418')
-    print(r.text)
-    return HttpResponse('<pre>' + r.text + '</pre>')
+    # driver.maximize_window()
+    #
+    # # Navigate to the application home page
+    #
+    #
+    # # get the search textbox
+    # search_field = driver.find_element_by_id("lst-ib")
+    # search_field.clear()
+    #
+    # # enter search keyword and submit
+    # search_field.send_keys("Selenium WebDriver Interview questions")
+    # search_field.submit()
+    #
+    # # get the list of elements which are displayed after the search
+    # # currently on result page using find_elements_by_class_name method
+    # lists= driver.find_elements_by_class_name("_Rm")
+    #
+    # # get the number of elements found
+    # print ("Found " + str(len(lists)) + " searches:")
+    #
+    # # iterate through each element and print the text that is
+    # # name of the search
+    #
+    # i=0
+    # for listitem in lists:
+    #    print (listitem.get_attribute("innerHTML"))
+    #    i=i+1
+    #    if(i>10):
+    #       break
+    #
+    # # close the browser window
+    # driver.quit()
+    #
+    # r = requests.get('http://httpbin.org/status/418')
+    # print(r.text)
+    # return HttpResponse('<pre>' + r.text + '</pre>')
     # return HttpResponse('Hello from Python!')
     # return render(request, "index.html")
 
